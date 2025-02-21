4a322f967694156e6557ad8e8e6fe2711f9a59a7
bennhub <bennhub@users.noreply.github.com>
2025-02-21 08:07:15 +0000

    deploy: 407faec7fde7f6f5445bee15c7d65dd939809f2e

---
+++ sw.js
@@ -0,0 +1,22 @@
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+      caches.open("groove-gallery-cache").then((cache) => {
+        return cache.addAll([
+        '/groove-slider-img/',  
+        '/groove-slider-img/index.html',
+        '/groove-slider-img/manifest.json',
+        '/groove-slider-img/icons/icon-192.png',
+        '/groove-slider-img/icons/icon-512.png'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener("fetch", (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
