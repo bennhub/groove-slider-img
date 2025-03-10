1a533b8f786dd38e08faffb6a93e3ac17623730c
bennhub <bennhub@users.noreply.github.com>
2025-03-10 00:13:00 +0000

    deploy: fc9102ec26b825ae4defbdfe12da8edce8e9dd69

--- sw.js
+++ sw.js
@@ -1,22 +1,48 @@
+const CACHE_NAME = "groove-gallery-cache-v1";
+
+const STATIC_ASSETS = [
+  '/groove-slider-img/',
+  '/groove-slider-img/index.html',
+  '/groove-slider-img/manifest.json',
+  '/groove-slider-img/icons/icon-192.png',
+  '/groove-slider-img/icons/icon-512.png',
+  'https://unpkg.com/@ffmpeg/core@0.12.9/dist/esm/ffmpeg-core.js', // Add FFmpeg JS core
+  'https://unpkg.com/@ffmpeg/core@0.12.9/dist/esm/ffmpeg-core.wasm', // Add FFmpeg WebAssembly file
+];
+
 self.addEventListener("install", (event) => {
-    event.waitUntil(
-      caches.open("groove-gallery-cache").then((cache) => {
-        return cache.addAll([
-        '/groove-slider-img/',  
-        '/groove-slider-img/index.html',
-        '/groove-slider-img/manifest.json',
-        '/groove-slider-img/icons/icon-192.png',
-        '/groove-slider-img/icons/icon-512.png'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener("fetch", (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(STATIC_ASSETS);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).then((networkResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          if (event.request.url.includes('/assets/')) {
+            cache.put(event.request, networkResponse.clone());
+          }
+          return networkResponse;
+        });
+      });
+    }).catch(() => caches.match('/groove-slider-img/index.html')) // Offline fallback
+  );
+});
+
+// Clear old caches when a new version is activated
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
